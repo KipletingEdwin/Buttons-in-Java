54d85755f5d086157029320652976061f3417a4a
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-02 08:35:59 +0300

    create a button and set bounds

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -3,6 +3,13 @@ import javax.swing.*;
 public class MyFrame extends JFrame {
 
     MyFrame(){
+
+        JButton button = new JButton();
+        button.setBounds(200,100,100,50);
+
+
+
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500, 500);
